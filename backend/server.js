d32e7ebc849cc0b6d602245bd382447378887674
Anant Kumar <angu22cs@cmrit.ac.in>
2025-06-21 17:46:13 +0530

    Fix Path

--- backend/server.js
+++ backend/server.js
@@ -23,16 +23,16 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-app.set('trust proxy', true);
-app.use(cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true
-}));
+app.set("trust proxy", true);
+app.use(
+	cors({
+		origin: process.env.CORS_ORIGIN,
+		credentials: true,
+	})
+);
 
-app.use(express.json({ limit: "5mb" })); // to parse req.body
-// limit shouldn't be too high to prevent DOS
-app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
-
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -41,10 +41,9 @@ app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
+	app.use(express.static(path.join(__dirname, "frontend", "dist")));
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+		res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 	});
 }
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -45,7 +45,7 @@ function App() {
 	}
 
 	return (
-		<div  data-theme="dark" className='flex max-w-6xl mx-auto'>
+		<div className='flex max-w-6xl mx-auto'>
 			{/* Common component, bc it's not wrapped with Routes */}
 			{authUser && <Sidebar />}
 			<Routes>
